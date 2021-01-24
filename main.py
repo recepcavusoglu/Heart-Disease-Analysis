0e328c7ec5e422a72ae14bdb5e6be38b6f743973
recepcvsgl26 <recepcvsgl26@gmail.com>
2021-01-24 12:41:09 +0300

    Classificaition Using Logistic Regression

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn import metrics
 
 #1- Veri setinde hastalıklı ve sağlam sayılarını sütun grafiği kullanarak çizdirin.
 def Hasta_Dagilim(p_df):
@@ -64,6 +67,40 @@ def Data_Preprocess(p_df):
     X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.20, random_state=1702)
     return X_train, X_test, y_train, y_test
 
+#7- Logistic Regresyon kullanarak veri setini sınıflandırın
+#(Sonucun karmaşıklık matrisini çizdirin, bu matrise göre Accuracy, Sensitivity, Specificity, Recall, Precision değerlendirme kriterlerini hesaplayın.) 
+def Logistic(p_trainX,p_testX,p_trainY,p_testY):
+    model=LogisticRegression()
+    model.fit(p_trainX,p_trainY)
+    y_pred = pd.Series(model.predict(p_testX))
+    p_testY = p_testY.reset_index(drop=True)
+    z = pd.concat([p_testY, y_pred], axis=1)
+    z.columns = ['True', 'Prediction']
+
+    cnf_matrix=metrics.confusion_matrix(p_testY,y_pred)
+    labels = [0, 1]
+    fig, ax = plt.subplots()
+    tick_marks = np.arange(len(labels))
+    plt.xticks(tick_marks, labels)
+    plt.yticks(tick_marks, labels)
+    # create heatmap
+    sns.heatmap(pd.DataFrame(cnf_matrix), annot=True, cmap="YlGnBu", fmt='g')
+    ax.xaxis.set_label_position("bottom")
+    plt.title('Confusion matrix')
+    plt.ylabel('True')
+    plt.xlabel('Predicted')
+
+    sensitivity=(cnf_matrix[1][1]/float(cnf_matrix[1][1]+cnf_matrix[1][0]))
+    specificity=(cnf_matrix[0][0]/float(cnf_matrix[0][0]+cnf_matrix[0][1]))
+
+    print("Accuracy:", metrics.accuracy_score(p_testY, y_pred))
+    print("Sensitivity:",sensitivity)
+    print("Specificity:",specificity)
+    print("Recall:", metrics.recall_score(p_testY, y_pred))
+    print("Precision:", metrics.precision_score(p_testY, y_pred))    
+    plt.show()
+
 if __name__=="__main__":
     df=pd.read_csv('data.csv')
     X_train, X_test, y_train, y_test=Data_Preprocess(df)
+    Logistic(X_train, X_test, y_train, y_test)
