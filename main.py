8c7a0727870dfdaef5ba390910ac9880c9be67bf
Recep Çavuşoğlu <33806246+recepcavusoglu@users.noreply.github.com>
2021-03-30 22:01:40 +0300

    Translate and commenting

--- main.py
+++ main.py
@@ -13,29 +13,29 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras.utils import to_categorical
 
-#1- Veri setinde hastalıklı ve sağlam sayılarını sütun grafiği kullanarak çizdirin.
-def Hasta_Dagilim(p_df):
+# Plots the Sickness Distribution
+def Sick_Distribution(p_df):
     status=[0,0]
     status[0]=len(p_df[p_df["target"]==1])
     status[1]=len(p_df)-status[0]
-    text="Hasta Sayısı: "+str(status[0])+" | Sağlam Sayısı: "+str(status[1])
-    labels=['Hasta','Hasta Değil']
+    text="Sick Count: "+str(status[0])+" | Healty Count: "+str(status[1])
+    labels=['Sick','Healty']
     y_pos = np.arange(len(labels))
     plt.bar(y_pos, status, align='center', alpha=0.5,color=['red','blue'])
     plt.xticks(y_pos, labels)
-    plt.title('Hastalık Dağılımı')
+    plt.title('Sick Distribution')
     plt.figtext(0.5, 0.01, text, ha="center", fontsize=10,bbox={"facecolor":"white","alpha":0.5, "pad":5})
     plt.show()
 
-#2- Cinsiyete göre hasta ve sağlıklı hasta sayıları sütun grafiği ile ifade ediniz. 
-def Hasta_Cinsiyet_Dagilim(p_df):
+#Plots the sickeness distrubution by gender
+def Sick_Gender_Distribution(p_df):
     male_count=len(p_df[p_df["sex"]==1])
     sick_male_count=len(p_df[(p_df["sex"]==1)&(p_df["target"]==1)])
     female_count=len(p_df[p_df["sex"]==0])
     sick_female_count=len(p_df[(p_df["sex"]==0)&(p_df["target"]==1)])
     #sick_male,healty_male,sick_female,healty_female
     status=[sick_male_count,male_count-sick_male_count,sick_female_count,female_count-sick_female_count]    
-    labels=['Erkek Hasta','Erkek Sağlıklı','Kadın Hasta','Kadın Sağlıklı']
+    labels=['Male Sick','Male Healty','Female Sick','Female Healty']
     text=""
     for i in range(len(labels)):
         text+=str(labels[i])+": "+str(status[i])+" | "
@@ -43,18 +43,16 @@ def Hasta_Cinsiyet_Dagilim(p_df):
     y_pos = np.arange(len(labels))
     plt.bar(y_pos, status, align='center', alpha=0.5,color=['red','blue','red','blue'])
     plt.xticks(y_pos, labels)
-    plt.title('Hastalık Cinsiyet Dağılımı')
+    plt.title('Sick Gender Distribution')
     plt.figtext(0.5, 0.01, text, ha="center", fontsize=10,bbox={"facecolor":"white","alpha":0.5, "pad":5})
     plt.show()
 
-#3- Veri setindekilerin yaş dağılımını gösteren bir sütun grafiği çizdirin.
-#4- Veri setinde hasta olanların yaş dağılımını gösteren bir sütun grafiği çizdirin.
-#İkisini tek methotda birleştirdim parametre olarak True gelirse 4, False gelirse 3 durumunu inceler.
-def Yas_Dagilimi(p_df,p_sick_status):
-    plt_title="Veri Seti Yaş Dağılımı"
+#Plots the age distribution if paramater true else plots  sickness age distribution
+def Age_Distribution(p_df,p_sick_status):
+    plt_title="Age Distribution"
     if p_sick_status==True:
         p_df=p_df[p_df["target"]==1]
-        plt_title="Hasta Yaş Dağılımı"
+        plt_title="Sick Age Distribution"
     age_distribution=[]
     age_distribution.append(len(p_df[p_df["age"]<30]))
     for i in range(30,70,10):
@@ -72,8 +70,7 @@ def Yas_Dagilimi(p_df,p_sick_status):
     plt.figtext(0.5, 0.01, text, ha="center", fontsize=10,bbox={"facecolor":"white","alpha":0.5, "pad":5})
     plt.show()
 
-#5- Veri setini sınıflandırma işlemi için hazırlayınız(Özelliklerin ölçeklenmesi, kategorik veri var ise nümerik hale getirilmesi, özelliklerin ve çıktının değişkenlere aktarılması)
-#6- Veri setini sınıflandırma algoritmalarında kullanmak üzere eğitim ve test olmak üzere ayırın. (%80 eğitim ,%20 test olacak şekilde)
+#Data Preprocess %20 test
 def Data_Preprocess(p_df):
     min_max_scaler=preprocessing.MinMaxScaler()
     data_scaled=min_max_scaler.fit_transform(p_df.values)
@@ -82,26 +79,9 @@ def Data_Preprocess(p_df):
     features=df.drop(["target"],axis=1)
     X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.20, random_state=256)
     return X_train, X_test, y_train, y_test
-    
-#7-Logistic Regresyon kullanarak veri setini sınıflandırın
-#8- K-NN kullanarak veri setini sınıflandırın
-#9- Naive Bayes kullanarak veri setini sınıflandırın
-#10- Karar ağaçları kullanarak veri setini sınıflandırın
-#(Sonucun karmaşıklık matrisini çizdirin, bu matrise göre Accuracy, Sensitivity, Specificity, Recall, Precision değerlendirme kriterlerini hesaplayın.)
-def Classifier(p_trainX,p_testX,p_trainY,p_testY,p_option):
-    if p_option=="Logistic":
-        model=LogisticRegression()
-    elif p_option=="KNN":
-        model=KNeighborsClassifier(n_neighbors=5)
-    elif p_option=="NaiveBayes":
-        model=GaussianNB()
-    elif p_option=="DecisionTree":
-        model=DecisionTreeClassifier()
-    elif p_option=="NeuralNet":
-        return Neural(p_trainX,p_testX,p_trainY,p_testY)      
 
-    model.fit(p_trainX,p_trainY)
-    y_pred=model.predict(X_test)
+# Creates heatmap and claculates metrics by test data prediction
+def Evaluate_Metrics(p_testY,y_pred,p_option):
     cnf_matrix=metrics.confusion_matrix(p_testY,y_pred)
     labels = [0, 1]
     fig, ax = plt.subplots()
@@ -125,16 +105,31 @@ def Classifier(p_trainX,p_testX,p_trainY,p_testY,p_option):
     recall=sensitivity
     precision=TP/(TP+FP)
 
-    print(p_option," Accuracy:", accuracy)
-    print(p_option," Sensitivity:",sensitivity)
-    print(p_option," Specificity:",specificity)
-    print(p_option," Recall:", recall)
-    print(p_option," Precision:", precision)    
+      
     plt.show()
+
     return (accuracy,sensitivity,specificity,recall,precision)
 
-#11- Yapay Sinir Ağları kullanarak veri setini sınıflandırın
-#(Sonucun karmaşıklık matrisini çizdirin, bu matrise göre Accuracy, Sensitivity, Specificity, Recall, Precision değerlendirme kriterlerini hesaplayın.)
+
+#Classifier for Logistic Reg, K-NN, Naive Bayes, Decision Tree
+def Classifier(p_trainX,p_testX,p_trainY,p_testY,p_option):
+    if p_option=="Logistic":
+        model=LogisticRegression()
+    elif p_option=="KNN":
+        model=KNeighborsClassifier(n_neighbors=5)
+    elif p_option=="NaiveBayes":
+        model=GaussianNB()
+    elif p_option=="DecisionTree":
+        model=DecisionTreeClassifier()
+    elif p_option=="NeuralNet":
+        return Neural(p_trainX,p_testX,p_trainY,p_testY)      
+
+    model.fit(p_trainX,p_trainY)
+    y_pred=model.predict(X_test)
+    return Evaluate_Metrics(p_testY,y_pred,p_option)
+    
+
+#Classifier for Neuralnet
 def Neural(p_trainX,p_testX,p_trainY,p_testY):
     dummy_y=p_testY
     p_trainY=to_categorical(p_trainY)
@@ -148,7 +143,7 @@ def Neural(p_trainX,p_testX,p_trainY,p_testY):
     model.add(Dense(2, activation='softmax'))
 
     model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-    model.fit(p_trainX, p_trainY, validation_data=(p_testX, p_testY), epochs=200, verbose=2)
+    model.fit(p_trainX, p_trainY, validation_data=(p_testX, p_testY), epochs=50, verbose=2)
     scores = model.evaluate(p_testX, p_testY)
     predictions = model.predict(p_testX)
     
@@ -156,36 +151,9 @@ def Neural(p_trainX,p_testX,p_trainY,p_testY):
     for i in predictions:
         y_pred.append(np.argmax(i))    
 
-    cnf_matrix=metrics.confusion_matrix(dummy_y,y_pred)
-    labels = [0, 1]
-    fig, ax = plt.subplots()
-    tick_marks = np.arange(len(labels))
-    plt.xticks(tick_marks, labels)
-    plt.yticks(tick_marks, labels)
-    sns.heatmap(pd.DataFrame(cnf_matrix), annot=True, cmap="YlGnBu", fmt='g')
-    ax.xaxis.set_label_position("bottom")
-    plt.title('NN Confusion matrix')
-    plt.ylabel('True')
-    plt.xlabel('Predicted')
+    return Evaluate_Metrics(dummy_y,y_pred,'NeuralNet')
 
-    TP=cnf_matrix[0][0]
-    FP=cnf_matrix[0][1]
-    FN=cnf_matrix[1][0]
-    TN=cnf_matrix[1][1]
-    accuracy=(TP+TN)/(TP+FP+TN+FN)
-    sensitivity=TP/(TP+FN)
-    specificity=TN/(TN+FP)
-    recall=sensitivity
-    precision=TP/(TP+FP)
-
-    print("NN Accuracy:", accuracy)
-    print("NN Sensitivity:",sensitivity)
-    print("NN Specificity:",specificity)
-    print("NN Recall:", recall)
-    print("NN Precision:", precision)   
-    plt.show()
-    return (accuracy,sensitivity,specificity,recall,precision)
-
+#Plots the metric scores
 def Plot(p_values,p_metric):
     if p_metric=="f1":
         f1=[]
@@ -201,7 +169,7 @@ def Plot(p_values,p_metric):
     
     ypos=np.arange(len(columns))
     plt.xticks(ypos,columns)
-    label=p_metric+" Değerleri"
+    label=p_metric+" Score"
     plt.ylabel(label)
     plt.title(p_metric)
     plt.bar(ypos,df.loc[p_metric].values,align='center', alpha=0.5,color=['red','blue','orange','purple','green'])
@@ -210,10 +178,10 @@ def Plot(p_values,p_metric):
 if __name__=="__main__":
     df=pd.read_csv('data.csv')
 
-    Hasta_Dagilim(df)
-    Hasta_Cinsiyet_Dagilim(df)
-    Yas_Dagilimi(df,False)
-    Yas_Dagilimi(df,True)
+    Sick_Distribution(df)
+    Sick_Gender_Distribution(df)
+    Age_Distribution(df,False)
+    Age_Distribution(df,True)
 
     X_train, X_test, y_train, y_test=Data_Preprocess(df)
     data=[X_train, X_test, y_train, y_test]
@@ -226,8 +194,10 @@ if __name__=="__main__":
     metric_values.append(Classifier(*data,"NeuralNet"))
     
     Plot(metric_values,"Accuracy")
+    '''
     Plot(metric_values,"Sensitivity")
     Plot(metric_values,"Specificity")
     Plot(metric_values,"Recall")
     Plot(metric_values,"Precision")
     Plot(metric_values,"f1")
+'''
